5004e5be3495b969b0762c31d7b86a7a83a70ad4
amirmub <amirmubarek01@gmail.com>
2025-04-13 17:08:43 +0300

    Add UpperFooter component with styling

--- amazone/src/App.jsx
+++ amazone/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./App.css"
 import Routing from './Router'
-
+import UpperFooter from './components/UpperFooter/UpperFooter'
 
 function App() {
   return (
     <div>
      <Routing />
+     <UpperFooter />
     </div>
   )
 }
---
+++ amazone/src/components/UpperFooter/UpperFooter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from '../../assets/images/logo.png'
+import { BsCurrencyDollar } from "react-icons/bs";
+import { FaGlobe } from "react-icons/fa";
+import flag from '../../assets/images/american_flag.png'
+import classes from './UpperFooter.module.css'
+
+function UpperFooter() {
+  return (
+    <div className={classes.upperFooter_Container}>
+      <div className={classes.upperFooter_logo}>
+          <a href="/"><img src={logo} /></a>
+      </div>
+
+      <div className={classes.upperFooter_Buttons}>
+          <button><FaGlobe className={classes.globe}/>English</button>
+          <button><BsCurrencyDollar className={classes.currency}/>USD-U.S.Dollar</button>
+          <button className={classes.footer_flag}><img src={flag}alt="flag" /> United State</button>
+      </div>
+    </div>
+  )
+}
+
+export default UpperFooter
