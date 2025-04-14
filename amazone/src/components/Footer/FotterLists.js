258513fc774863e41e60afb6b5e06d7372b34664
amirmub <amirmubarek01@gmail.com>
2025-04-14 11:02:05 +0300

    Add additional footer list items for enhanced content

--- amazone/src/components/Footer/FotterLists.js
+++ amazone/src/components/Footer/FotterLists.js
@@ -5,4 +5,34 @@ export const footerLists = [
         col3 : "Kindle Direct Publishing Indie Digital & Print Publishing Made Easy",
         col4 : "eero WiFi Stream 4K Video in Every Room"
     },
+    {
+        col1 : "6pmScore dealson fashion brands",
+        col2 : "Amazon Web ServicesScalable Cloud Computing Services",
+        col3 : "Prime Video DirectVideo Distribution Made Easy",
+        col4 : "Blink Smart Security for Every Home"
+    },
+    {
+        col1 : "AbeBooks Books, art & collectibles",
+        col2 : "Audible Listen to Books & Original Audio Performances",
+        col3 : "Shopbop Designer Fashion Brands",
+        col4 : "Neighbors App Real-Time Crime & Safety Alerts"
+    },
+    {
+        col1 : "ACX Audiobook Publishing Made Easy",
+        col2 : "Box Office Mojo Find Movie Box Office Data",
+        col3 : "Woot! Deals and Shenanigan",
+        col4 : "Amazon Subscription Boxes Top subscription boxes – right to your door"
+    },
+    {
+        col1 : "Sell on Amazon Start a Selling Account",
+        col2 : "Goodreads Book reviews & recommendations",
+        col3 : "Zappos Shoes & Clothing",
+        col4 : "PillPackPharmacy Simplified"
+    },
+    {
+        col1 : "Amazon Business Everything For Your Business",
+        col2 : "Amazon Second Chance Pass it on, trade it in, give it a second life",
+        col3 : "Amazon Photos Unlimited Photo Storage Free With Prime",
+        col4 : "Amazon Drive Cloud storage from Amazon"
+    }
 ]
