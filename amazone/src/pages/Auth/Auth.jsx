c02cf3d0f84f918e6d3336444288d8723cf2d57b
amirmub <amirmubarek01@gmail.com>
2025-04-23 16:56:55 +0300

    Fix logo link and update  text in Auth component

--- amazone/src/pages/Auth/Auth.jsx
+++ amazone/src/pages/Auth/Auth.jsx
@@ -7,7 +7,7 @@ function Auth() {
   return (
       <section className={classes.auth_section}>
         <div className={classes.auth_container}>
-        <Link><img src={logo2} alt="logo" /></Link>
+        <Link to="/"><img src={logo2} alt="logo" /></Link>
 
           <div className={classes.auth_content}>
               <h1>Sign-in</h1>
@@ -17,8 +17,8 @@ function Auth() {
               <input type="password" /><br />
               <button>SignIn</button>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi praesentium, quae eveniet perferendis quis dolores 
-                quo nesciunt sint! Earum ipsa soluta ad nostrum, velit eos praesentium animi totam ipsam repellat!
+                 By signing in you agree to the AMAZONE FACE CLONE Contribution of use and Sale
+                 . Please See Our Privacy Notice, our Cookies Notice and Our Interest Based Ad Notice.
               </p>
               <button>Create Your Amazone Account</button>
           </div>
