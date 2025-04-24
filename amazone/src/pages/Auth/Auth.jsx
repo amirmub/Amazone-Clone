f8badceaff99f37a3e9542d219f23d5df2ee5d7f
amirmub <amirmubarek01@gmail.com>
2025-04-24 17:32:01 +0300

    Enhance authentication component with loading states and success/error messages

--- amazone/src/pages/Auth/Auth.jsx
+++ amazone/src/pages/Auth/Auth.jsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom'
 import logo2 from "../../assets/images/logo2.png"
 import classes from "./Auth.module.css"
 import {auth} from "../../Utility/firebase"
-import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth"
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} from "firebase/auth"
 import { DataContext } from '../../components/DataProvider/DataProvider'
 import { ActionTypes } from '../../Utility/action.type'
+import {ClipLoader} from "react-spinners"
 
 function Auth() {
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
   const [error,setError] = useState("")
+  const [success, setSuccess] = useState(false);
+  const [loading,setLoading] = useState({ signIn : false, signOut : false})
 
   const [{user},dispatch] = useContext(DataContext)
    console.log(user);
@@ -21,6 +24,7 @@ function Auth() {
     e.preventDefault()
     // console.log(e.target.name);
     if (e.target.name === "signIn") {
+      setLoading({...loading,signIn : true})
       signInWithEmailAndPassword(auth,email,password)
       .then((userInfo) =>{
         // console.log(userInfo);
@@ -28,12 +32,18 @@ function Auth() {
           type : ActionTypes.SET_USER,
           user : userInfo.user
         })
+        setSuccess(true);
+        setError("");
+        setLoading({...loading,signIn : false})
       })
       .catch((err) =>{
         console.log(err);
+        setError(err.message)
+        setLoading({...loading,signIn : false})
       })
     }
     else{
+      setLoading({...loading,signOut : true})
       createUserWithEmailAndPassword(auth,email,password)
       .then((userInfo) =>{
         // console.log(userInfo);
@@ -41,9 +51,14 @@ function Auth() {
           type : ActionTypes.SET_USER,
           user : userInfo.user
         })
+        setLoading({...loading,signOut : false})
+        setSuccess(true);
+        setError("");
       })
       .catch((err) =>{
         console.log(err);
+        setError(err.message)
+        setLoading({...loading,signOut : false})
       })
     }
     
@@ -62,12 +77,27 @@ function Auth() {
               <label htmlFor="password">Password</label><br />
               <input value={password} onChange={(e) => setPassword((e.target.value))} type="password" /><br />
 
-              <button type='submit' onClick={authHandler} name='signIn'>SignIn</button>
+              <button type='submit' onClick={authHandler} name='signIn'>
+                 {
+                    loading.signIn ? <ClipLoader  size={18}/> :  "SignIn"
+                 }
+              </button> 
                 <p>
                   By signing in you agree to the AMAZONE FACE CLONE Contribution of use and Sale
                   . Please See Our Privacy Notice, our Cookies Notice and Our Interest Based Ad Notice.
                 </p>
-              <button  type='submit' onClick={authHandler} name='signUp'>Create Your Amazone Account</button>
+              <button  type='submit' onClick={authHandler} name='signUp'>
+                 {
+                    loading.signOut ? <ClipLoader  size={18}/> :  "Create Your Amazone Account"
+                 }
+              </button>
+              {
+                error && <small style={{ color: "red" }}>{error}</small>
+              }
+              {
+                 success && !error && <small style={{ color: "green" }}>Successfully Register</small>
+              }
+
           </div>
       </div>
     </section>
