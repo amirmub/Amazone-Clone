4644fce49a3e9064c85be322a3fdb3c03aecb2fb
amirmub <amirmubarek01@gmail.com>
2025-04-26 19:52:36 +0300

    Initialize package.json for amazone_backend with essential dependencies and start script

--- amazone/functions/index.js
+++ amazone/functions/index.js
@@ -8,3 +8,35 @@ const dotenv = require("dotenv");
 dotenv.config();//uses for READ inside .env file
 const stripe = require("stripe") (process.env.STRIPE_SECRET_KEY);
 
+const app = express()
+app.use(cors({origin : true})) //uses for accept from any request
+
+app.use(express.json())
+
+app.get("/",(req,res) =>{
+    res.status(200).json({message : "successfully register"})
+})
+
+
+app.post("/payment/create",async(req,res) =>{
+    const total = req.query.total
+
+      if (total > 0) {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount : total,
+            currency : "USD"
+         })
+         res.status(201).json({
+            clientSecret : paymentIntent.client_secret,
+         })
+         console.log(paymentIntent);
+      }
+        else{
+           res.status(403).json({
+            message : "total must be greatethan 0"
+           })
+
+        }
+    })
+
+exports.api = onRequest(app)
