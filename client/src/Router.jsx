3e1599cb1ca1d8d3b407417907020bb46ae7b1da
amirmub <amirmubarek01@gmail.com>
2025-04-27 23:31:12 +0300

    Add Stripe integration for payment processing and update styles

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -7,6 +7,12 @@ import Payment from './pages/Payment/Payment'
 import Results from './pages/Results/Results'
 import Cart from './pages/Cart/Cart'
 import ProductDetail from './pages/ProductDetail/ProductDetail'
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(
+  'pk_test_51RHXl5Q19mhqDu1YP0j8sY75Qre6MBdxn47spQTpmsrBhkSR00Nb7rNOPfa9WSCyRKyY3XZwpZwvWrjOiv9tInEi00HRPOLTgL'
+);
 
 function Routing() {
   return (
@@ -16,7 +22,12 @@ function Routing() {
          <Route path="/" element={<Landing />} />
          <Route path="/auth" element={<Auth />} />
          <Route path="/order" element={<Order />} />
-         <Route path="/payments" element={<Payment />} />
+         <Route path="/payments" element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+           } 
+          />
          <Route path="/category/:catagoryName" element={<Results />} />
          <Route path="/products/:productId" element={<ProductDetail />} />
          <Route path="/cart" element={<Cart />} />
--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -3,11 +3,46 @@ import LayOut from '../../components/LayOut/LayOut'
 import { DataContext } from '../../components/DataProvider/DataProvider'
 import ProductCard from '../../components/Product/ProductCard'
 import classes from "./payment.module.css"
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
 
 function Payment() {
-  const [products, setProducts] = useState({});
   const [{basket,user},dispatch] = useContext(DataContext);
   const totalItems = basket?.reduce((acc, item) => acc + item.quantity, 0);
+  const [cardError,setCarError] = useState("");
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+   const handleChange = (e)=>{
+    setCarError(e.error.message)
+   }
+
+  // const [loading, setLoading] = useState(false);
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   if (!stripe || !elements) return;
+
+  //   setLoading(true);
+
+  //   const { clientSecret } = await res.json();
+
+  //   const result = await stripe.confirmCardPayment(clientSecret, {
+  //     payment_method: {
+  //       card: elements.getElement(CardElement),
+  //     },
+  //   });
+
+  //   if (result.error) {
+  //     console.error(result.error.message);
+  //   } else {
+  //     if (result.paymentIntent.status === 'succeeded') {
+  //       alert('Payment successful!');
+  //     }
+  //   }
+
+  //   setLoading(false);
+  // };
   return (
     <LayOut>
       <section className={classes.payment_container}>
@@ -34,7 +69,6 @@ function Payment() {
                         <ProductCard 
                           data={item} key={item.id}
                           flex = {true} 
-                          product_description = {true} 
                           cart_button = {false} 
                           cart_style={true} 
                           />
@@ -45,8 +79,20 @@ function Payment() {
           </div>
           <hr style={{border:"1.4px solid #E3E6E6"}}/>
 
-        <div>
+        <div className={classes.payment_card_container}>
           <h3>Payment Method</h3>
+          <div className={classes.payment_card}>
+           <form>
+            {
+              cardError && <small style={{color : "red",marginLeft : "10px"} }>{cardError}</small>
+            }
+              <CardElement onChange={handleChange} />
+              <p>Total Order | ${basket.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)}</p>
+            <button onClick={(e)=>{e.preventDefault()}} type="submit">
+             Pay Now
+            </button>
+           </form>
+          </div>
         </div>
         </section>
       </section>
