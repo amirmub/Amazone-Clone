eb9d5e3cc39bf8ad756c8e3cab1c943e9c08e1b7
amirmub <amirmubarek01@gmail.com>
2025-04-27 11:01:43 +0300

    Refactor Payment component

--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import LayOut from '../../components/LayOut/LayOut'
+import { DataContext } from '../../components/DataProvider/DataProvider'
+import ProductCard from '../../components/Product/ProductCard'
+import classes from "./payment.module.css"
 
 function Payment() {
+  const [products, setProducts] = useState({});
+  const [{basket,user},dispatch] = useContext(DataContext);
+  const totalItems = basket?.reduce((acc, item) => acc + item.quantity, 0);
   return (
     <LayOut>
-      Payment
+      <section className={classes.payment_container}>
+         {/* header */}
+        <div className={classes.payment_header}>Checkout ({totalItems} ) items</div>
+
+        {/* payment method */}
+        <section style={{marginBottom : "90vh"}}>
+          <h3>Delivery Address</h3>
+            <div>
+              <p>{user?.email}</p>
+              <p>Megenagna</p>
+              <p>Addiss,Ababa Ethiopia</p>
+            </div>
+          <div>
+            <h3>Review Item and delivery</h3>
+            { 
+              basket.map((item) => (   
+                  <section className={classes.cart_item_section}>
+                    <ProductCard 
+                      data={item} key={item.id}
+                      flex = {true} 
+                      product_description = {true} 
+                      cart_button = {false} 
+                      cart_style={true} 
+                      />
+                </section> 
+               ))
+              }
+          </div>
+        </section>
+      </section>
     </LayOut>
   )
 }
