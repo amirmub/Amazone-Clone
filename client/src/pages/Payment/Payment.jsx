bc1601bb510c2e414e17766b3911dec6501ee069
amirmub <amirmubarek01@gmail.com>
2025-04-27 11:37:23 +0300

    refactore styling for payment page

--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -12,32 +12,42 @@ function Payment() {
     <LayOut>
       <section className={classes.payment_container}>
          {/* header */}
-        <div className={classes.payment_header}>Checkout ({totalItems} ) items</div>
+        <div className={classes.payment_header}>Checkout ({totalItems}) items</div>
 
         {/* payment method */}
-        <section style={{marginBottom : "90vh"}}>
-          <h3>Delivery Address</h3>
-            <div>
-              <p>{user?.email}</p>
-              <p>Megenagna</p>
-              <p>Addiss,Ababa Ethiopia</p>
-            </div>
-          <div>
-            <h3>Review Item and delivery</h3>
-            { 
-              basket.map((item) => (   
-                  <section className={classes.cart_item_section}>
-                    <ProductCard 
-                      data={item} key={item.id}
-                      flex = {true} 
-                      product_description = {true} 
-                      cart_button = {false} 
-                      cart_style={true} 
-                      />
-                </section> 
-               ))
-              }
+        <section>
+           <div className={classes.payment_address}>
+              <h3>Delivery Address</h3>
+                <div>
+                  <p>{user?.email}</p>
+                  <p>Megenagna</p>
+                  <p>Addiss,Ababa Ethiopia</p>
+                </div>
+           </div>
+           <hr style={{border:"1.4px solid #E3E6E6"}}/>
+          <div className={classes.payment_item}>
+            <h3>Review Item and Delivery</h3>
+             <div>
+                { 
+                  basket.map((item) => (   
+                      <section className={classes.cart_item_section}>
+                        <ProductCard 
+                          data={item} key={item.id}
+                          flex = {true} 
+                          product_description = {true} 
+                          cart_button = {false} 
+                          cart_style={true} 
+                          />
+                    </section> 
+                  ))
+                  }
+              </div>
           </div>
+          <hr style={{border:"1.4px solid #E3E6E6"}}/>
+
+        <div>
+          <h3>Payment Method</h3>
+        </div>
         </section>
       </section>
     </LayOut>
