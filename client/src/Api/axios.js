034e04ef9ee8bb0469db9074594845b5671fb9a3
amirmub <amirmubarek01@gmail.com>
2025-05-06 11:41:34 +0300

    fix: update axios baseURL to correct endpoint

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -2,5 +2,6 @@ import axios from "axios"
 
 export  const axiosInstance = axios.create({
     // baseURL : "http://127.0.0.1:5001/e-1277a/us-central1/api"
-    baseURL : "https://amazone-clone-78qz.onrender.com"
+    baseURL : "https://amazone-clone-1-7e0v.onrender.com"
+    
 })
